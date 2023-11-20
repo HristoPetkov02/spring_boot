395d045fdb9114b08f38f9f231113d30cb0b3563
LlamaLamap1 <126472619+LlamaLamap1@users.noreply.github.com>
2023-11-20 18:49:33 +0200

    Initial commit

---
+++ src/main/java/sit/tuvarna/bg/first_app/FirstAppApplication.java
@@ -0,0 +1,13 @@
+package sit.tuvarna.bg.first_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sit/tuvarna/bg/first_app/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.tuvarna.bg.first_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
